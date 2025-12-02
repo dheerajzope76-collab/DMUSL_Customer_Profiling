5a31d45555350b2455c1afe302c270105f4488f6
dheerajzope76-collab <dheerajzope76@gmail.com>
2025-12-02 19:12:35 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-from app import main
-
-if __name__ == "__main__":
-    # For local debugging you can also do:
-    #   streamlit run app.py
-    # but this allows a simple `python main.py` entry as well.
-    main()
