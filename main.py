7ef8db7e6c68ab50b88bfb2342e15860a054d2ba
dheerajzope76-collab <dheerajzope76@gmail.com>
2025-12-02 18:40:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from app import main
+
+if __name__ == "__main__":
+    # For local debugging you can also do:
+    #   streamlit run app.py
+    # but this allows a simple `python main.py` entry as well.
+    main()
